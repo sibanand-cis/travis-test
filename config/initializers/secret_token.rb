3b5ab5f277b840259a1f825171fb8b3269514446
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-04-24 19:13:29 +0530

    t6est for travis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravisTest::Application.config.secret_token = '9517b6e07c1a461284b95de1f4e5d08bfcd3eebed33d21d16d9eef07afb5af9f71d1fed84d8f4659fba5a73124efbc1ec2a5d5af4375e0842b997cf4797f81e2'
